5d7967301bbea62659332d434f9db63e27153311
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-01 10:29:57 +0530

    this is the first commit

---
+++ app/src/main/java/com/example/retrofit_app_today_day_/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.retrofit_app_today_day_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
